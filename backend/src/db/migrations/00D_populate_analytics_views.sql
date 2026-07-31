6f8973fc909fe12fcf15bd263d0e18707b3e624e
zenoguy <ghoshshreyan04042005@gmail.com>
2026-08-01 00:42:54 +0530

    ci: use initial non-concurrent refresh in 00D_populate_analytics_views.sql

--- backend/src/db/migrations/00D_populate_analytics_views.sql
+++ backend/src/db/migrations/00D_populate_analytics_views.sql
@@ -1,4 +1,8 @@
--- Migration 00D: Initial population of analytics materialized views
--- Ensures project_health_mv, zone_performance_mv, executive_kpi_mv, etc. are populated upon setup.
+-- Migration 00D: Initial non-concurrent population of analytics materialized views
+-- PostgreSQL requires an initial non-concurrent refresh before CONCURRENTLY can be used.
 
-SELECT public.refresh_analytics_views();
+REFRESH MATERIALIZED VIEW public.project_health_mv;
+REFRESH MATERIALIZED VIEW public.zone_performance_mv;
+REFRESH MATERIALIZED VIEW public.executive_kpi_mv;
+REFRESH MATERIALIZED VIEW public.budget_leakage_mv;
+REFRESH MATERIALIZED VIEW public.monthly_cashflow_trends_mv;
